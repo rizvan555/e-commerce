0b656ffe2b85ddb32cf46de76277ab7bd8aa1090
Rizvan <rizvan@MacBook-Air-2.fritz.box>
2023-09-10 23:25:06 +0200

    created categoryPage and get forms from bootstrap

---
+++ frontend/src/app/components/categories/categories.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SharedModule } from 'src/app/common/shared/shared.module';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-categories',
+  standalone: true,
+  imports: [SharedModule],
+  templateUrl: './categories.component.html',
+  styleUrls: ['./categories.component.css'],
+})
+export class CategoriesComponent {}
