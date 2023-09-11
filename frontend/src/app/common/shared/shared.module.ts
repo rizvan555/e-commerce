c5c6ebb9ba2174682aac6584c5ad3652bff2cb3b
Rizvan <rizvan@MacBook-Air-2.fritz.box>
2023-09-11 19:11:54 +0200

    added generik BLANK component and created titles and names for components

--- frontend/src/app/common/shared/shared.module.ts
+++ frontend/src/app/common/shared/shared.module.ts
@@ -3,10 +3,23 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { ValidDirective } from '../directives/valid.directive';
+import { BlankComponent } from '../components/blank/blank.component';
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, RouterModule, FormsModule, ValidDirective],
-  exports: [CommonModule, RouterModule, FormsModule, ValidDirective],
+  imports: [
+    CommonModule,
+    RouterModule,
+    FormsModule,
+    ValidDirective,
+    BlankComponent,
+  ],
+  exports: [
+    CommonModule,
+    RouterModule,
+    FormsModule,
+    ValidDirective,
+    BlankComponent,
+  ],
 })
 export class SharedModule {}
